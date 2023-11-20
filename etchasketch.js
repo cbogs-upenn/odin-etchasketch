51efad046e59102c69f5dddc8deb27c861a5ac54
Christopher Bogs <cbogs@upenn.edu>
2023-11-19 21:29:14 -0500

    Implement cell loop

--- etchasketch.js
+++ etchasketch.js
@@ -1,2 +1,22 @@
 'use strict';
 
+let cellLabel = "";
+
+const container = document.querySelector("#container");
+
+// set up a 16 x 16 grid of square divs
+
+for (let row = 0; row <= 15; row++){
+    
+    for (let column = 0; column <= 15; column++){
+
+        cellLabel = "r" + row + "c" + column; // label will be r0c0, r0c1, etc.
+
+        const cell = document.createElement('div');
+            cell.setAttribute("id", cellLabel);
+            cell.classList.add("cell");
+            
+            container.appendChild(cell);
+    }
+
+}
